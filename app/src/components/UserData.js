558ae6c8f298db4bb2aba3734c1ae34a25d8b69d
Lina Kichen <hkichen@gmail.com>
2018-09-09 19:10:33 -0400

    tallies in console log

--- app/src/components/UserData.js
+++ app/src/components/UserData.js
@@ -7,13 +7,20 @@ class UserData extends Component {
     this.state = { 
       userName : "",
       userPic : "",
-      userCommits: []
+      userCommits: [],
+      pullEvent: 0,
+      watchEvent: 0,
+      pullRequestEvent: 0,
+      createEvent:0,
+      publicEvent: 0
+
     };
   }
   
   componentDidMount() {
     axios.get('https://api.github.com/users/hkichen/events')
       .then(res => {
+        //getting the user name and pic
         var searchedName = res.data[0].actor.display_login;
         var searchedPic = res.data[0].actor.avatar_url;
         this.setState({ 
@@ -21,6 +28,7 @@ class UserData extends Component {
           userPic : searchedPic 
         })
 
+        //getting the commits
         var justCommits = [];
         var data = res.data;
         data.forEach(function(event) {
@@ -31,13 +39,39 @@ class UserData extends Component {
             })
           }
         })
-        console.log(justCommits);
-        var just3 = [];
+        //console.log(justCommits);
+        let just3 = [];
         just3.push(justCommits[0])
         just3.push(justCommits[1])
         just3.push(justCommits[2])
-        console.log(just3)
+        //console.log(just3)
         this.setState({userCommits:just3})
+
+        //getting all events 
+        //console.log(data)
+        var events = []
+        data.forEach(function(event) {
+          // console.log(event.type)
+          events.push(event.type)
+        })
+        
+        events.sort();
+        console.log(events);
+
+        //counting event types
+        var current = null;
+        var cnt = 0;
+        for (var i = 0; i <= events.length; i++) {
+          if (events[i] !== current) {
+            if (cnt > 0) {
+              console.log(current + ' occured ' + cnt + ' time(s)');
+            }
+            current = events[i];
+            cnt = 1;
+          } else {
+              cnt++;
+          }
+      }
     })
   }
 
@@ -45,6 +79,10 @@ class UserData extends Component {
     return (
       <div>
         <div> GitHub Api Challenge</div>
+        <div className="searchBar">
+          <input placeholder="enter a username" />
+          <button>Search</button>
+        </div>
         <h2>User Name: {this.state.userName}</h2>
         <img alt="userPic" src={this.state.userPic} />
         <ul>
